f9095d7c2466f31ec344c672bdbff56999fad762
eStefan <stefansaravolac@gmail.com>
2015-07-21 21:36:07 +0200

    Added redirect to thanks.html

--- scripts/hubspot.php
+++ scripts/hubspot.php
@@ -5,15 +5,18 @@
 		$HAPIKey = '18996f4a-fb67-4f8a-8f91-6ba88e5a9f56';
 		
 		$email = $_POST["email"]; //GET AN EMAIL FROM FORM
-		print_r($email);
+		//print_r($email);
 		$contacts = new HubSpot_Contacts($HAPIKey);
     
 			
 			//Create Contact
 		$params =  array('email' => $email, 'firstname' => 'John' );
 		$createdContact = $contacts->create_contact($params);
-		print_r($createdContact);
+		//print_r($createdContact);
 		$newly_created_vid = $createdContact->{'vid'};
+		
+		header("Location: http://tiinyhouse.com/thanks.html"); /* Redirect browser */
+		exit();
 		
 	/*		
 		//Update Contact
