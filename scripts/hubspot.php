b9a6b33575c76d6e08dcf354c8261739ac5da16d
eStefan <stefansaravolac@gmail.com>
2015-07-21 22:37:08 +0200

    Added put url data to input fields.

--- scripts/hubspot.php
+++ scripts/hubspot.php
@@ -4,13 +4,65 @@
 		
 		$HAPIKey = '18996f4a-fb67-4f8a-8f91-6ba88e5a9f56';
 		
-		$email = $_POST["email"]; //GET AN EMAIL FROM FORM
+		if( isset($_POST['zipcode']) ){
+			$zipcode = $_POST["zipcode"]; 
+		
+		} else{
+			$zipcode = '';
+		}
+		
+		if( isset($_POST['eco']) ){
+			$eco = $_POST["eco"]; 
+		
+		} else{
+			$eco = '';
+		}
+		
+		if( isset($_POST['assembly']) ){
+			$assembly = $_POST["assembly"]; 
+			
+		} else{
+			$assembly = '';
+		}
+		
+		if( isset($_POST['swapping']) ){
+			$swapping = $_POST["swapping"]; 
+			
+		} else{
+			$swapping = '';
+		}
+		
+		if( isset($_POST['cost']) ){
+			$cost = $_POST["cost"]; 
+			
+		} else{
+			$cost = '';
+		}
+		
+		if( isset($_POST['days']) ){
+			$days = $_POST["days"]; 
+			
+		} else{
+			$days = '';
+		}
+						
+		$email = $_POST["email"]; 
+		
+		
 		//print_r($email);
 		$contacts = new HubSpot_Contacts($HAPIKey);
     
 			
 			//Create Contact
-		$params =  array('email' => $email, 'firstname' => '' );
+		$params =  array(
+						 'firstname' 	=> '', 
+						 'zipcode' 		=> $zipcode, 
+						 'eco' 			=> $eco,
+						 'assembly' 	=> $assembly,
+						 'swapping' 	=> $swapping,
+						 'cost' 		=> $cost,
+						 'days' 		=> $days,
+						 'email' 		=> $email);
 		$createdContact = $contacts->create_contact($params);
 		//print_r($createdContact);
 		$newly_created_vid = $createdContact->{'vid'};
