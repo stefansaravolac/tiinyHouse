c09f8b9a9c1e36fbcd9f6e1b3cc1da85c28f9aa3
eStefan <stefansaravolac@gmail.com>
2015-07-21 21:05:07 +0200

    Fixed email var sent to hubspot

--- scripts/hubspot.php
+++ scripts/hubspot.php
@@ -5,13 +5,12 @@
 		$HAPIKey = '18996f4a-fb67-4f8a-8f91-6ba88e5a9f56';
 		
 		$email = $_POST["entry.518332116"]; //GET AN EMAIL FROM FORM
-		echo "SENT EMAIL IS: " + $email;
-		
+		print_r($email);
 		$contacts = new HubSpot_Contacts($HAPIKey);
     
 			
 			//Create Contact
-		$params =  array('email' => $contacts, 'firstname' => 'John' );
+		$params =  array('email' => $email, 'firstname' => 'John' );
 		$createdContact = $contacts->create_contact($params);
 		print_r($createdContact);
 		$newly_created_vid = $createdContact->{'vid'};
